4ff5d16af53b639cc2899743b32b7108261cfc9c
garrettspot <shoheadam@gmail.com>
2026-05-03 02:13:23 +0530

    remove set import

--- src/test/java/com/adam/coursemanager/service/CourseServiceTest.java
+++ src/test/java/com/adam/coursemanager/service/CourseServiceTest.java
@@ -15,7 +15,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
